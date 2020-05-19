90c46aa4cf506da2ea12cfb118fb2793e955bd44
Yanjun <yanjun.xie@outlook.com>
2020-05-19 18:22:42 -0400

    Revert "Initial commit"
    This reverts commit 74d93ba4854f0c1cd2ceb2bb8d5f719cea1d59ca.

--- server/organizations/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import Organization
-
-class OrganizationSerializer(serializers.HyperlinkedModelSerializer):
-
-    class Meta:
-        model = Organization
-        fields = ['id', 'name', 'address_line1', 'address_line2', 'address_city', 'address_state', 'address_zip', 'address_country', 'phone',]
