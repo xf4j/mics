0816d1b0c07b13fccd144af134ae3f285472a218
shivanidewan <dewan.shivani19@gmail.com>
2020-07-29 14:10:15 -0400

    runfile updated

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ mkdir -p /storage/orthanc
 # Start gunicorn
 # To-do: check the system core and set the number of workers correspondingly
 mkdir -p /storage/mics
-cp -n /mics/server/db.deploy.sqlite3 /storage/mics/db.deploy.sqlite3
+cp -n /mics/server/db.deploy.sqlite3 /storage/mics/db.sqlite3
 mkdir -p /storage/gunicorn
 cd /mics/server
 gunicorn -w 4 -t 3600 --access-logfile /storage/gunicorn/access --error-logfile /storage/gunicorn/error server.wsgi &
